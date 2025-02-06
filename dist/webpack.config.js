da86796618fa208fc018b1b4d63bc6c26282b9b8
Tatyana N <mairon.angband@gmail.com>
2025-02-06 19:24:10 +0300

    add files

---
+++ dist/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+modele.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
