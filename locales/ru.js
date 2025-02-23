1e21e88d0e7326749f62b542fd5f038c31342462
Tatyana N <mairon.angband@gmail.com>
2025-02-23 22:25:48 +0300

    Add validate links

---
+++ locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    validate: {
+      urlValidate: 'Ссылка должна быть валидным URL',
+      rssRepeat: 'RSS уже существует',
+      rssSuccess: 'RSS успешно загружен',
+      rssRequired: 'Заполните это поле.',
+      rssRequest: 'Ресурс не содержит валидный RSS',
+    },
+  },
+};
---
+++ src/app.js
@@ -0,0 +1,83 @@
+import * as yup from 'yup';
+import i18next, { t } from 'i18next';
+import resources from '../locales/index.js';
+
+await i18next.init({
+  lng: 'ru',
+  resources,
+});
+
+const existingRssLinks = [];
+
+const schema = yup.object().shape({
+  url: yup.string()
+    .required(t('validate.rssRequired'))
+    .url(t('validate.urlValidate'))
+    .test('is-unique', t('validate.rssRepeat'), (value) => {
+      if (!value) return true;
+      return !existingRssLinks.includes(value);
+    }),
+});
+
+const elements = {
+  button: document.querySelector('[aria-label="add"]'),
+  input: document.querySelector('#url-input'),
+  form: document.querySelector('.rss-form'),
+  feedback: document.querySelector('.feedback'),
+};
+
+const renderError = (errorMessage, success) => {
+  elements.feedback.classList.remove('text-danger', 'text-success');
+  elements.feedback.classList.add(success ? 'text-success' : 'text-danger');
+  elements.feedback.textContent = errorMessage;
+};
+
+const requestRss = (urlNames) => {
+  const { input } = elements;
+  fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(urlNames)}`, { cache: 'no-store' })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(t('validate.rssRequest'));
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const { contents } = data;
+
+      const isValidRss = /<rss[\s\S]*?>|<feed[\s\S]*?>/.test(contents);
+      if (!isValidRss) {
+        throw new Error(t('validate.rssRequest'));
+      }
+
+      existingRssLinks.push(urlNames);
+      input.classList.add('is-valid');
+      renderError(t('validate.rssSuccess'), true);
+      input.value = '';
+      input.focus();
+    })
+    .catch((err) => {
+      renderError(err.message, false);
+    });
+};
+
+const app = () => {
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const { input } = elements;
+
+    const urlValue = input.value;
+    input.classList.remove('is-invalid', 'is-valid');
+
+    schema.validate({ url: urlValue })
+      .then(() => {
+        requestRss(urlValue);
+      })
+      .catch((err) => {
+        input.classList.add('is-invalid');
+        renderError(err.message, false);
+      });
+  });
+};
+
+export default app;
