de616fe9a05807bc243bb3e9f6406eca0ce257c1
Tatyana N <mairon.angband@gmail.com>
2025-03-20 20:09:12 +0300

    test

--- src/app.js
+++ src/app.js
@@ -79,11 +79,6 @@ const app = async () => {
       .then((data) => {
         const { contents } = data;
 
-        const isValidRss = /<rss[\s\S]*?>|<feed[\s\S]*?>/.test(contents);
-        if (!isValidRss) {
-          throw new Error(t('validate.rssRequest'));
-        }
-
         existingRssLinks.push(urlNames);
         lastInformationOfFeeds[urlNames] = contents;
         input.classList.add('is-valid');
