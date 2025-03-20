52f4ab85983c2b87a3a968ddcd25319591a629c7
Tatyana N <mairon.angband@gmail.com>
2025-03-20 05:20:21 +0300

    fix button

--- src/app.js
+++ src/app.js
@@ -11,35 +11,65 @@ await i18next.init({
   resources,
 });
 
-const existingRssLinks = [];
-const lastInformationOfFeeds = {};
+const app = () => {
+  const existingRssLinks = [];
+  const lastInformationOfFeeds = {};
 
-const schema = yup.object().shape({
-  url: yup.string()
-    .required(t('validate.rssRequired'))
-    .url(t('validate.urlValidate'))
-    .test('is-unique', t('validate.rssRepeat'), (value) => {
-      if (!value) return true;
-      return !existingRssLinks.includes(value);
-    }),
-});
+  const schema = yup.object().shape({
+    url: yup.string()
+      .required(t('validate.rssRequired'))
+      .url(t('validate.urlValidate'))
+      .test('is-unique', t('validate.rssRepeat'), (value) => {
+        if (!value) return true;
+        return !existingRssLinks.includes(value);
+      }),
+  });
 
-const elements = {
-  button: document.querySelector('[aria-label="add"]'),
-  input: document.querySelector('#url-input'),
-  form: document.querySelector('.rss-form'),
-  feedback: document.querySelector('.feedback'),
-};
+  const elements = {
+    button: document.querySelector('[aria-label="add"]'),
+    input: document.querySelector('#url-input'),
+    form: document.querySelector('.rss-form'),
+    feedback: document.querySelector('.feedback'),
+  };
 
-const renderError = (errorMessage, success) => {
-  elements.feedback.classList.remove('text-danger', 'text-success');
-  elements.feedback.classList.add(success ? 'text-success' : 'text-danger');
-  elements.feedback.textContent = errorMessage;
-};
+  const renderError = (errorMessage, success) => {
+    elements.feedback.classList.remove('text-danger', 'text-success');
+    elements.feedback.classList.add(success ? 'text-success' : 'text-danger');
+    elements.feedback.textContent = errorMessage;
+  };
 
-const startCheckingUpdates = (url) => {
-  const checkForUpdates = () => {
-    fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`, { cache: 'no-store' })
+  const startCheckingUpdates = (url) => {
+    const checkForUpdates = () => {
+      fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`, { cache: 'no-store' })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(t('validate.rssRequest'));
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const { contents } = data;
+
+          if (lastInformationOfFeeds[url] !== contents) {
+            lastInformationOfFeeds[url] = contents;
+            createPostElements(data);
+          }
+        })
+        .catch((err) => {
+          if (err.message === 'Failed to fetch') {
+            renderError(t('validate.failedToFetch'), false);
+          } else {
+            console.error(err.message);
+          }
+        });
+      setTimeout(checkForUpdates, 5000);
+    };
+    checkForUpdates();
+  };
+
+  const requestRss = (urlNames) => {
+    const { input } = elements;
+    fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(urlNames)}`, { cache: 'no-store' })
       .then((response) => {
         if (!response.ok) {
           throw new Error(t('validate.rssRequest'));
@@ -49,65 +79,34 @@ const startCheckingUpdates = (url) => {
       .then((data) => {
         const { contents } = data;
 
-        if (lastInformationOfFeeds[url] !== contents) {
-          lastInformationOfFeeds[url] = contents;
-          createPostElements(data);
+        const isValidRss = /<rss[\s\S]*?>|<feed[\s\S]*?>/.test(contents);
+        if (!isValidRss) {
+          throw new Error(t('validate.rssRequest'));
         }
+
+        existingRssLinks.push(urlNames);
+        lastInformationOfFeeds[urlNames] = contents;
+        input.classList.add('is-valid');
+        renderError(t('validate.rssSuccess'), true);
+        input.value = '';
+        input.focus();
+
+        createPost();
+        createFeeds();
+        createPostElements(data);
+        createFeedsElements(data);
       })
       .catch((err) => {
-        if (err.message === 'Failed to fetch') {
-          renderError(t('validate.failedToFetch'), false);
-        } else {
-          console.error(err.message);
-        }
+        input.classList.add('is-invalid');
+        const errorMessage = err.message === 'Failed to fetch' ? t('validate.failedToFetch') : err.message;
+        renderError(errorMessage, false);
+      })
+      .finally(() => {
+        startCheckingUpdates(urlNames);
       });
-    setTimeout(checkForUpdates, 5000);
   };
-  checkForUpdates();
-};
 
-const requestRss = (urlNames) => {
-  const { input } = elements;
-  fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(urlNames)}`, { cache: 'no-store' })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(t('validate.rssRequest'));
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const { contents } = data;
-
-      const isValidRss = /<rss[\s\S]*?>|<feed[\s\S]*?>/.test(contents);
-      if (!isValidRss) {
-        throw new Error(t('validate.rssRequest'));
-      }
-
-      existingRssLinks.push(urlNames);
-      lastInformationOfFeeds[urlNames] = contents;
-      input.classList.add('is-valid');
-      renderError(t('validate.rssSuccess'), true);
-      input.value = '';
-      input.focus();
-
-      createPost();
-      createFeeds();
-      createPostElements(data);
-      createFeedsElements(data);
-    })
-    .catch((err) => {
-      input.classList.add('is-invalid');
-      const errorMessage = err.message === 'Failed to fetch' ? t('validate.failedToFetch') : err.message;
-      renderError(errorMessage, false);
-    })
-    .finally(() => {
-      startCheckingUpdates(urlNames);
-    });
-};
-
-const app = () => {
-  const { form } = elements;
-  form.addEventListener('submit', async (e) => {
+  elements.form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const { input } = elements;
@@ -115,13 +114,14 @@ const app = () => {
     const urlValue = input.value;
     input.classList.remove('is-invalid', 'is-valid');
 
-    try {
-      await schema.validate({ url: urlValue });
-      requestRss(urlValue);
-    } catch (err) {
-      input.classList.add('is-invalid');
-      renderError(err.message, false);
-    }
+    schema.validate({ url: urlValue })
+      .then(() => {
+        requestRss(urlValue);
+      })
+      .catch((err) => {
+        input.classList.add('is-invalid');
+        renderError(err.message, false);
+      });
   });
 };
 
